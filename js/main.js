057f5a4f9a12bdf269a050cc780da8639ec98258
Fenn Sann <122974847+FennSann@users.noreply.github.com>
2023-01-18 18:36:08 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,25 @@
+function yes(){
+	document.write(`<style>
+			.zhut {
+				width: 200px;
+				height: 200px;
+			}
+			.center {
+				text-align: center;
+			}
+		</style>
+		<p class="center">可是人家户籍都被公开一般了捏</p><br>
+		<p class="center">姓名 主殷豪</p><br>
+		<p class="center">住址 江苏扬州</p><br>
+		<p class="center">母亲 居红香</p><br>
+		<p class="center">手机 13626120571</p><br>
+		<p class="center"><img src="img/zhuyinhao.png" class="zhut"/></p>`)
+}
+function no() {
+	document.write(`<style>
+			.center {
+				text-align: center;
+			}
+		</style>
+		<p class="center">滚出网易大神去，这不是你撒野的地方</p>`)
+}
